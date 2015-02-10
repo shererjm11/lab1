c30e6562bcc9a9119130f49bf47da9489e2a589f
shererjm11 <shererjm11@gmail.com>
2015-02-10 01:08:06 -0600

    lab1.0

---
+++ guessinggame.java
@@ -0,0 +1,35 @@
+package Lab1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class guessinggame {
+
+	public static void main(String[] args) {
+		Random num = new Random();
+		int numguess = num.nextInt(10);
+		Scanner input = new Scanner(System.in);
+		int guess;
+		int numTries = 0;
+		boolean win = false;
+		
+			while(win == false){
+		
+			System.out.println("Guess a number between 1 and 10: ");
+			guess = input.nextInt();
+			numTries++;	
+		
+			if(guess == numguess){
+				win = true;
+			}else{
+				System.out.println("Wrong! Guess again!");
+			}
+			
+			
+		
+		}
+			System.out.println("You Won");
+			
+
+	}
+}
